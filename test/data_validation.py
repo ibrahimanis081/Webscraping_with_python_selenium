1168e13e49f4033499eebdb807e24f57395fd61e
Anees <ibrahimanis081@gmail.com>
2023-08-19 10:02:56 +0100

    add great_expectations to requirements.txt, expand validation to be more robust, secure database credentials in lambder function

--- test/data_validation.py
+++ test/data_validation.py
@@ -1,5 +1,7 @@
 import configparser
+from logging import exception
 import great_expectations as gx
+from great_expectations.checkpoint import Checkpoint
 
 # Read the configuration
 config = configparser.ConfigParser()
@@ -12,41 +14,63 @@ db_username = config['database']['username']
 db_password = config['database']['password']
 db_name = config['database']['database_name']
 
-connection_string = (
+MYSQL_CONNECTION_STRING = (
     f"mysql+mysqlconnector://{db_username}:{db_password}@{db_host}:{db_port}/{db_name}"
 )
 
 context = gx.get_context()
 print("Context created")
 
-context.add_or_update_expectation_suite("my_expectation_suite")
-
-datasource = context.sources.add_sql(
-    name="my_datasource", connection_string=connection_string
+try:
+    MySQL_datasource = context.sources.add_sql(
+    name="MySQL_datasource", 
+    connection_string=MYSQL_CONNECTION_STRING
 )
+    print("Connected to DataSource")
+except Exception as e:
+    print(F"Unable to connect to database {e}")
+   
 
-print("connected to data source")
 
-datasource = context.get_datasource("my_datasource")
-table_asset = datasource.add_table_asset(name="my_asset", table_name="us_accredited_online_colleges")
-batch_request = table_asset.build_batch_request()
+MySQL_datasource.add_table_asset(
+    name="us_accredited_online_colleges",
+    table_name="us_accredited_online_colleges"
+)
+batch_request = MySQL_datasource.get_asset("us_accredited_online_colleges").build_batch_request()
 
+expectation_suite_name = "mysql_expectation_suite"
+
+context.add_or_update_expectation_suite(expectation_suite_name=expectation_suite_name)
 validator = context.get_validator(
     batch_request=batch_request,
-    expectation_suite_name="my_expectation_suite",
+    expectation_suite_name=expectation_suite_name,
 )
 
+# expectations
+validator.expect_column_to_exist(column="serial_no")
+validator.expect_column_values_to_be_unique(column="serial_no")
+validator.expect_column_values_to_not_be_null(column="tuition")
+validator.expect_table_column_count_to_be_between(5, 10)
+validator.expect_column_values_to_be_of_type("tuition", "INTEGER")
+validator.save_expectation_suite(discard_failed_expectations=False)
 
-validator.expect_column_values_to_not_be_null(column="s/n")
-column_check = validator.expect_table_column_count_to_equal(5)
+my_checkpoint_name = "mysql_checkpoint"
+checkpoint = Checkpoint(
+    name=my_checkpoint_name,
+    run_name_template="%Y%m%d-%H%M%S-my-run-name-template",
+    data_context=context,
+    batch_request=batch_request,
+    expectation_suite_name=expectation_suite_name,
+    action_list=[
+        {
+            "name": "store_validation_result",
+            "action": {"class_name": "StoreValidationResultAction"},
+        },
+        {"name": "update_data_docs", "action": {"class_name": "UpdateDataDocsAction"}},
+    ],
+)
 
-print(column_check)
+context.add_or_update_checkpoint(checkpoint=checkpoint)
+checkpoint_result = checkpoint.run()
 
-validator.save_expectation_suite()
-
-
-context.build_data_docs()
 context.open_data_docs()
-
-
-
